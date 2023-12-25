53d505530d6e44ba5496505b2bc5cee3c5db01d1
VictorPxx <147117528+VictorPxx@users.noreply.github.com>
2023-12-25 14:03:11 -0300

    initial commit

---
+++ assets/js/variaveis.js
@@ -0,0 +1,11 @@
+let idade = 31;
+let nome = 'Victor';
+let sobrenome = 'Luz';
+let cidade = 'Aracaju';
+let estado = 'SE';
+let verdade = true;
+
+console.log(typeof idade + ' - ' + idade);
+console.log(typeof nome  + ' - ' + nome + ' '+ sobrenome);
+console.log(typeof cidade + ' - ' + cidade + '/' + estado);
+console.log(typeof verdade + ' - ' + verdade)
